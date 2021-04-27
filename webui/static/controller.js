be5b96b39b2f0f1541d58a7d57269e63cc2f6fd3
Paweł Woźny <pawelpwozny@gmail.com>
2021-04-27 22:13:16 +0200

    Set simple UI with selectable values

--- webui/static/controller.js
+++ webui/static/controller.js
@@ -1,4 +1,4 @@
-const config = {
+let config = {
   br: 0,
   r: 0,
   g: 0,
@@ -6,7 +6,32 @@ const config = {
 };
 
 function update(event, slider) {
-  config[slider] = event.target.value;
-  document.querySelector('#result').innerText = JSON.stringify(config);
+  if (event) {
+    config[slider] = event.target.value;
+  }
+  document.querySelector(
+    '#selected-color'
+  ).style.background = `rgba(${config.r}, ${config.g}, ${config.b}, ${config.br})`;
+
+  document.querySelector(
+    '#result'
+  ).innerText = `rgba(\n${config.r}, \n${config.g}, \n${config.b}, \n${config.br})`;
+
   fetch(`set?br=${config.br}&r=${config.r}&g=${config.g}&b=${config.b}`);
+}
+
+function setFavorite(r, g, b, br) {
+  config = {
+    r,
+    g,
+    b,
+    br,
+  };
+
+  document.querySelector('#r').value = r;
+  document.querySelector('#g').value = g;
+  document.querySelector('#b').value = b;
+  document.querySelector('#br').value = br;
+
+  update();
 }
